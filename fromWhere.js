88bdcd196d590dcb99275c198d5a12fdaefba855
Londiwe <celelondiwe3@gmail.com>
2018-03-26 15:53:25 +0200

    made changes to fromWhere functions

--- fromWhere.js
+++ fromWhere.js
@@ -1,12 +1,12 @@
 var fromWhere = function(regNum){
   switch(regNum){
-    case "CY":
+    case "CY 324":
       return "Bellville";
      break;
-    case "CJ":
+    case "CJ 123":
       return "Paarl";
      break;
-    case "CA":
+    case "CA 987":
       return "Cape Town";
      break;
     default :
@@ -15,7 +15,7 @@ var fromWhere = function(regNum){
          }
 };
 
-assert.equal(fromWhere("CY"),"Bellville");
-assert.equal(fromWhere("CJ"),"Paarl");
-assert.equal(fromWhere("CA"),"Cape Town");
-assert.equal(fromWhere("NUZ"),"Some other place!");
+assert.equal(fromWhere("CY 324"),"Bellville");
+assert.equal(fromWhere("CJ 123"),"Paarl");
+assert.equal(fromWhere("CA 987"),"Cape Town");
+assert.equal(fromWhere("NUZ 476"),"Some other place!");
--- tests/fromWhere.test.js
+++ tests/fromWhere.test.js
@@ -1,23 +1,23 @@
 describe('fromWhere', function(){
     //var count = 'morning, afternoon, nightshift';
 
-    it('it should return Bellville for CY input', function(){
-      assert.equal(fromWhere("CY"),"Bellville");
+    it('it should return Bellville registration number starting with CY', function(){
+      assert.equal(fromWhere("CY 324"),"Bellville");
     });
 
-    it('it should return Paarl for CJ input.', function(){
+    it('it should return Paarl registration number starting with CJ.', function(){
         // this test will fail - can you fix it?
-      assert.equal(fromWhere("CJ"),"Paarl");
+      assert.equal(fromWhere("CJ 123"),"Paarl");
     });
 
-    it('it should return Capetown for CA input.', function(){
+    it('it should return Capetown registration number starting with CA.', function(){
         // this test will fail - can you fix it?
-        assert.equal(fromWhere("CA"),"Cape Town");
+        assert.equal(fromWhere("CA 987"),"Cape Town");
     });
 
     it('should return unapplicacable if input is not a registration number.', function(){
         // this test will fail - can you fix it?
-        assert.equal(fromWhere("NUZ"),"Some other place!");
+        assert.equal(fromWhere("NUZ 476"),"Some other place!");
     });
 
 
